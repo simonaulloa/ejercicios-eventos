099854eac405f3b4521431ef7c066bd8f679694b
simona <simonaulloa@gmail.com>
2017-05-16 17:59:58 -0400

    casi que casi

--- asset/main.js
+++ asset/main.js
@@ -1,7 +1,15 @@
 function demo(){
 	var boton = document.getElementById("demo");
-	boton.addEventListener("click", function(){ /*agregamos evento al hacer clic, eliminar o agregar tachado contenedor*/
+	boton.addEventListener("click", function(){ 
     alert("HOLA MUNDO!!!");
 })
 }
 demo();
+
+function img(){
+	var imagen = document.getElementById("img");
+	imagen.addEventListener("mouseover", function(){ 
+    alert("NO ESTAS SOBRE MI");
+})
+}
+img();
