bfcc0d1b882ecfa75efc2d27d09a9ed469df273b
simona <simonaulloa@gmail.com>
2017-05-16 16:29:14 -0400

    siii! boton de hola mundo!

---
+++ asset/main.js
@@ -0,0 +1,7 @@
+function demo(){
+	var boton = document.getElementById("demo");
+	boton.addEventListener("click", function(){ /*agregamos evento al hacer clic, eliminar o agregar tachado contenedor*/
+    alert("HOLA MUNDO!!!");
+})
+}
+demo();
